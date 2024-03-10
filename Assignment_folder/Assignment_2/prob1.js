4ea7df05290bc9456ef3356f3b69eee62b9f14c2
surajshivabla1@gmail.com <surajshivabla1@gmail.com>
2024-03-10 11:40:56 +0530

    Adding file prob1.js

---
+++ Assignment_folder/Assignment_2/prob1.js
@@ -0,0 +1,20 @@
+//1. [Variables] Declare different types of variables and print the type of each variable?
+
+let n = 5
+console.log("n is",typeof n)
+
+let s1 = "Suraj"
+console.log("s1 is",typeof s1)
+
+let arr = [1,2,3,4,5]
+console.log("arr is",typeof arr)
+
+let bool = true
+console.log("bool is",typeof bool)
+
+
+/* output
+n is number
+s1 is string
+arr is object
+bool is boolean   */
