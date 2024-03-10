f6395133e192a9edc93b7bd2b8207f1e1638cf1b
surajshivabla1@gmail.com <surajshivabla1@gmail.com>
2024-03-10 10:48:09 +0530

    Changes done in prob8.js

---
+++ Assignment_folder/Assignment_1/prob8.js
@@ -0,0 +1,13 @@
+// 8. Implement string reversal. dont use builtin function
+
+let str = "Suraj"
+let rev_str =""
+for(let i=str.length-1;i>=0;i--)
+{
+    rev_str = rev_str + str[i]
+}
+
+console.log(rev_str)
+
+//Output :  jaruS
+
