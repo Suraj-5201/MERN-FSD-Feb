4b65761c08a3708ed6c738805b75152a886790fe
surajshivabla1@gmail.com <surajshivabla1@gmail.com>
2024-03-10 13:19:43 +0530

    Changes done in prob3.js

--- Assignment_folder/Assignment_2/prob3.js
+++ Assignment_folder/Assignment_2/prob3.js
@@ -1,3 +1,5 @@
+//3. Create a calculator using function for each operartions -  sum, subraction, multiplication and division of 2 integers provided by user
+
 const readline = require('readline')
 
 let rl = readline.createInterface({
