83ae151b02b82da8e616c619342cf26bdc4911be
surajshivabla1@gmail.com <surajshivabla1@gmail.com>
2024-03-16 18:52:33 +0530

    Edited in p1.js

--- QA_session/QA01_16-03-24/p1.js
+++ QA_session/QA01_16-03-24/p1.js
@@ -2,8 +2,6 @@ let arr = [-7,1,5,2,-4,3,0]
 let cum_arr = []
 cum_arr[0]=arr[0]
 let flag=0
-//let sum1=0
-let sum2=0
 
 for(let i=1;i<arr.length;i++)
 {
@@ -11,49 +9,36 @@ for(let i=1;i<arr.length;i++)
 }
 //console.log(cum_arr)
 
-/*function findInd(arr)
+function findminInd(arr)
 {
-    for(let i=1;i<arr.length;i++)
+    for(let i=1;i<arr.length-1;i++)
     {
-        let ls = cum_arr[i-1]
-        let rs = cum_arr[cum_arr.length-1]-cum_arr[i]
-        if(ls==rs)
-         {
+        let sum1=0
+        let sum2=0
+        for(let l=0;l<i;l++)
+        {
+            sum1 = sum1 + arr[l]
+        }
+        for(let r=arr.length-1;r>i;r--)
+        {
+              sum2 = sum2 + arr[r]
+        }
+        //console.log(sum1,sum2)
+
+        if(sum1==sum2)
+        {
             flag=1
-            console.log(arr[i])
+            console.log(i)
             break
         }
     }
     if(flag==0)
     {
         console.log(-1)
-    }
-    
-}
-
-findInd(arr)
-*/
-
-function findminInd(arr)
-{
-    //let sum1=0
-    //let sum2=0
-
-    for(let k=0;k<arr.length-1;k++)
-    {
-        let sum1=0
-        let sum2=0
-        for(let i=k;i<arr.length;i++)
-        {
-            sum1 = sum1 + arr[i]
-        }
-        for(let j=arr.length-1;j>k;j--)
-        {
-              sum2 = sum2 + arr[j]
-        }
-        console.log(sum1,sum2)
     } 
     
 }
 
 findminInd(arr)
+
+//OUTPUT 3
