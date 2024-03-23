fea50836abfa037983ec1f3d312501beb14d857d
surajshivabla1@gmail.com <surajshivabla1@gmail.com>
2024-03-23 16:59:44 +0530

    Edited in p1.js in QA

---
+++ QA_session/QA03_23-03-24/p1.js
@@ -0,0 +1,20 @@
+/*
+Number of 1 Bits
+Problem Description
+Write a function that takes an integer and returns the number of 1 bits it has.
+Input Format
+First and only argument contains integer A
+Output Format
+Return an integer as the answer
+Example Input
+Input1:
+21
+Example Output
+Output1:
+3
+Example Explanation
+Explaination1:
+21 is represented as 10101 in binary.
+*/
+
+
