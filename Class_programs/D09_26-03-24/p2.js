b0de2773645ba96bff514909cb19553f39de7cd0
surajshivabla1@gmail.com <surajshivabla1@gmail.com>
2024-03-26 10:48:41 +0530

    Edited in p2.js in class_programs

---
+++ Class_programs/D09_26-03-24/p2.js
@@ -0,0 +1,48 @@
+//Queue
+class queue{
+    constructor(capacity)
+    {
+        this.myArray = []
+        this.capacity=capacity
+    }
+
+    nQueue(element)
+    {
+        if(this.myArray.length<this.capacity)
+        {
+            this.myArray.push(element)
+        }
+        return -1
+    }
+
+    dQueue()
+    {
+        if(this.myArray.length!=0)
+        {
+            this.myArray.shift()
+        }
+        else{
+            console.log("Queue is empty")
+        }
+    }
+
+    display()
+    {
+        console.log(this.myArray)
+    }
+}
+
+let arr1 = new queue(3)
+arr1.nQueue(1)
+arr1.nQueue(2)
+arr1.nQueue(3)
+arr1.nQueue(4)
+arr1.nQueue(5)
+arr1.display()
+
+/*arr1.dQueue()
+arr1.dQueue()
+arr1.dQueue()
+arr1.dQueue()
+arr1.dQueue()
+arr1.display()*/
