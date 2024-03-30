ce231fd7606a8978abe4abeb67fdb45ab0834fe7
surajshivabla1@gmail.com <surajshivabla1@gmail.com>
2024-03-30 11:27:35 +0530

    edited in p1.js in QA

---
+++ QA_session/QA05_30-03-24/p1_max_num_brutforce.js
@@ -0,0 +1,20 @@
+//Finding the Maximum - Develop a recursive function to find the maximum number in an array.
+
+let arr = [1,3,2,4,5,9]
+
+
+function max(arr)
+{
+    let max = Number.MIN_VALUE
+    let temp=0
+    for(let i=0;i<arr.length;i++)
+    {
+        if(max<arr[i])
+        {
+            max = arr[i]
+        }
+    }
+    return max
+}
+
+console.log(max(arr))
