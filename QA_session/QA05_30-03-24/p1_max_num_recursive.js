0543de4f6eab18a34c6dea5aa0d7c15ea1313abe
surajshivabla1@gmail.com <surajshivabla1@gmail.com>
2024-03-31 19:06:13 +0530

    edited in p1.js in QA

--- QA_session/QA05_30-03-24/p1_max_num_recursive.js
+++ QA_session/QA05_30-03-24/p1_max_num_recursive.js
@@ -1,2 +1,20 @@
 //Finding the Maximum - Develop a recursive function to find the maximum number in an array.
 
+function findmax(arr,n){
+    if(n===1){
+        return arr[0];
+
+    }
+    let max=findmax(arr,n-1);
+
+    if(arr[n-1]>max){
+        return arr[n-1];
+    }
+    else{
+        return max
+    }
+}
+
+let arr1=[2,9,7,8];
+let maxnum=findmax(arr1,arr1.length);
+console.log(maxnum);
